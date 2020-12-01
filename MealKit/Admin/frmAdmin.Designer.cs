1c05e64363b4c27e1aa107b199f6836e6b712df9
정준호 <yks3858@gmail.com>
2020-12-01 18:25:53 +0900

    초기 커밋.

---
+++ MealKit/Admin/frmAdmin.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace MealKit
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.제품등록ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.이벤트등록ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bOM관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.매출ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.사용자주문현황ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.발주ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.입고ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.회원관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.곹옹코드관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.제품등록ToolStripMenuItem,
+            this.이벤트등록ToolStripMenuItem,
+            this.bOM관리ToolStripMenuItem,
+            this.매출ToolStripMenuItem,
+            this.사용자주문현황ToolStripMenuItem,
+            this.발주ToolStripMenuItem,
+            this.입고ToolStripMenuItem,
+            this.회원관리ToolStripMenuItem,
+            this.곹옹코드관리ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 제품등록ToolStripMenuItem
+            // 
+            this.제품등록ToolStripMenuItem.Name = "제품등록ToolStripMenuItem";
+            this.제품등록ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.제품등록ToolStripMenuItem.Text = "제품 등록";
+            // 
+            // 이벤트등록ToolStripMenuItem
+            // 
+            this.이벤트등록ToolStripMenuItem.Name = "이벤트등록ToolStripMenuItem";
+            this.이벤트등록ToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.이벤트등록ToolStripMenuItem.Text = "이벤트등록";
+            // 
+            // bOM관리ToolStripMenuItem
+            // 
+            this.bOM관리ToolStripMenuItem.Name = "bOM관리ToolStripMenuItem";
+            this.bOM관리ToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.bOM관리ToolStripMenuItem.Text = "BOM관리";
+            // 
+            // 매출ToolStripMenuItem
+            // 
+            this.매출ToolStripMenuItem.Name = "매출ToolStripMenuItem";
+            this.매출ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.매출ToolStripMenuItem.Text = "매출";
+            // 
+            // 사용자주문현황ToolStripMenuItem
+            // 
+            this.사용자주문현황ToolStripMenuItem.Name = "사용자주문현황ToolStripMenuItem";
+            this.사용자주문현황ToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.사용자주문현황ToolStripMenuItem.Text = "사용자주문현황";
+            // 
+            // 발주ToolStripMenuItem
+            // 
+            this.발주ToolStripMenuItem.Name = "발주ToolStripMenuItem";
+            this.발주ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.발주ToolStripMenuItem.Text = "발주";
+            // 
+            // 입고ToolStripMenuItem
+            // 
+            this.입고ToolStripMenuItem.Name = "입고ToolStripMenuItem";
+            this.입고ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.입고ToolStripMenuItem.Text = "입고";
+            // 
+            // 회원관리ToolStripMenuItem
+            // 
+            this.회원관리ToolStripMenuItem.Name = "회원관리ToolStripMenuItem";
+            this.회원관리ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.회원관리ToolStripMenuItem.Text = "회원관리";
+            // 
+            // 곹옹코드관리ToolStripMenuItem
+            // 
+            this.곹옹코드관리ToolStripMenuItem.Name = "곹옹코드관리ToolStripMenuItem";
+            this.곹옹코드관리ToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.곹옹코드관리ToolStripMenuItem.Text = "공통코드관리";
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmAdmin";
+            this.Text = "frmAdmin";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 제품등록ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 이벤트등록ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bOM관리ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 매출ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 사용자주문현황ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 발주ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 입고ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 회원관리ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 곹옹코드관리ToolStripMenuItem;
+    }
+}
---
+++ MealKit/Customer/CustomerInfo/frmCustomerInfo.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MealKit.Customer
+{
+    partial class frmCustomerInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(403, 159);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 63);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "휴면계정전환";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 63);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "주문내역";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "환불내역";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "수정";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmCustomerInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmCustomerInfo";
+            this.Text = "frmCustomerInfo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
